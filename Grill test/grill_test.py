fced9eb6c5d4d449dbcc3ad610119bd47ef432b2
nataliiazinchenko <nataellie@gmail.com>
2022-08-31 12:14:41 +0300

    "positive functional tests: meat type" are added

--- Grill test/grill_test.py
+++ Grill test/grill_test.py
@@ -4,6 +4,14 @@ import pytest
 
 class TestGrill:
 #Positive scenarios
-#Functional tests
-    def test_correct_meat_type(self):
+#Functional tests: meat type
+    def test_correct_meat_type_newyork(self):
         assert "New York" in business.grill("New York", 5)
+    def test_correct_meat_type_ribeye(self):
+        assert "Ribeye" in business.grill("Ribeye", 10)
+    def test_correct_meat_type_tenderloin(self):
+        assert "Tenderloin" in business.grill("Tenderloin", 20)
+    def test_correct_meat_type_porterhous(self):
+        assert "Porterhouse" in business.grill("Porterhouse", 20)
+
+    
